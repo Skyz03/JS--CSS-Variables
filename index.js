f18400515c1bd78eeb85d02ea284c66ccf02fcbe
Skyz03 <aakibdiscoo@gmail.com>
2022-01-05 23:31:55 +0545

    Updates with the comments

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const inputs = document.querySelectorAll(".controls input");
+const inputs = document.querySelectorAll(".controls input");  //This is the selector for all the inputs.
 
 function handleUpdate() {
-  const suffix = this.dataset.sizing || '';
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+  const suffix = this.dataset.sizing || '';  // This is getting the suffix for sizing i.e. px.
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)  // This is setting the property as we are selecting the document property and changing its value as per we get from the input above.
 
   console.log(this.value);
 }
 
+
+// These are the codes that are lisiting to the change and updating the value by calling the function handleUpdate.
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
 inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
